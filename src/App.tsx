00c84c4ad5a86d638ec5d7b3a7a42d1752aa9f29
habtesh-beki <habteshbaki2014@gmail.com>
2025-03-22 15:28:09 -0400

    finished desktop view part

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,29 @@
 import ImageBg from "./assets/profile.jpg";
+import { Copyright } from "lucide-react";
 import MyExpertise from "./components/MyExpertise";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import MyWork from "./components/MyWork";
+import {
+  firstLineImagae,
+  SecondLineImagae,
+  ThiredLineImagae,
+} from "./utils/ImageExport";
+import { Button } from "./components/ui/button";
 
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 gsap.defaults({ ease: "none", duration: 2 });
 
-const Projects = [
-  {
-    img: "/portifolio/2.png",
-    title: "AASTU Gebi-gubye Student Management App",
-    desc: "We've done orthodox student management system for addis abeba science and technology university",
-    frontend: "https://github.com/habtesh-beki/aastu_gebi_gubaye_frontend.git",
-    backend: "https://github.com/HaileabT/aastu_gibi_gubaye_api.git",
-  },
-  {
-    img: "/portifolio/13.png",
-    title: "Ticketing System",
-    desc: "I've done Ticketing system user can create ticket and admin can update the status of it",
-    frontend: "",
-    backend: "",
-  },
-  {
-    img: "/portifolio/16.png",
-    title: "Simple blogging app",
-    desc: "We've done blogging app user can create blog and read others blog",
-    frontend: "",
-    backend: "",
-  },
-];
-
 export default function App() {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
   const container = useRef(null);
   const scrollRef = useRef(null);
+  const logoRef = useRef(null);
+  const hoverAnimation = useRef<HTMLDivElement | null>(null);
+  const containerFooter = useRef(null);
 
   useGSAP(
     () => {
@@ -60,6 +48,42 @@ export default function App() {
     { scope: scrollRef }
   );
 
+  useGSAP(
+    () => {
+      const t2 = gsap.timeline();
+      const t3 = gsap.timeline();
+      const t4 = gsap.timeline();
+      t2.to(".first", { xPercent: -20, duration: 1 });
+      t3.to(".second", { xPercent: 20, duration: 1 });
+      t4.to(".thried", {
+        xPercent: -20,
+        duration: 1,
+      });
+
+      ScrollTrigger.create({
+        animation: t2,
+        trigger: ".logo-animation",
+        scrub: true,
+        start: "top center",
+        end: "+=500",
+      });
+      ScrollTrigger.create({
+        animation: t3,
+        trigger: ".logo-animation",
+        scrub: true,
+        start: "top center",
+        end: "+=500",
+      });
+      ScrollTrigger.create({
+        animation: t4,
+        trigger: ".logo-animation",
+        scrub: true,
+        start: "top center",
+        end: "+=500",
+      });
+    },
+    { scope: logoRef }
+  );
   useGSAP(
     () => {
       gsap.from(".good", { y: -300, duration: 1 });
@@ -95,142 +119,238 @@ export default function App() {
 
     { scope: container }
   );
+  useGSAP(
+    () => {
+      gsap.to(".footer-open-to-work", {
+        boxShadow: "0 0 20px 10px rgba(39, 214, 85, 0.7)",
+        opacity: 1,
+        duration: 0.6,
+        ease: "power1.inOut",
+        repeat: -1,
+        yoyo: true,
+      });
+    },
+    { scope: containerFooter }
+  );
 
-  // useEffect(() => {
-  //   const handleMouseMove = (e) => {
-  //     setCursourPosition({ x: e.clientX, y: e.clientY });
-  //   };
-  //   window.addEventListener("mousemove", handleMouseMove);
-  //   return () => {
-  //     window.removeEventListener("mousemove", handleMouseMove);
-  //   };
-  // }, []);
+  useEffect(() => {
+    const handleMouseMove = (e: any) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    };
 
-  // useEffect(() => {
-  //   const handleScrollY = () => {
-  //     setScrollPosition(window.scrollY);
-  //   };
+    window.addEventListener("mousemove", handleMouseMove);
 
-  //   window.addEventListener("scroll", handleScrollY);
-  // });
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    const timeline = gsap.timeline();
+    if (hoverAnimation.current) {
+      timeline.to(hoverAnimation.current, {
+        x: cursorPosition.x,
+        y: cursorPosition.y,
+        duration: 0.3,
+        ease: "power2.out",
+        overwrite: false,
+      });
+    }
+  }, [cursorPosition]);
+
+  useEffect(() => {});
 
   return (
-    <div
-      className={`w-full h-auto bg-[url('./assets/bg-image.svg')] bg-no-repeat bg-cover p-0 overflow-hidden`}
-    >
-      <div className="flex flex-col w-full h-full items-center  covered-bg">
-        <div className="fixed left-1/2 flex justify-center transform -translate-x-1/2 w-fit z-1000">
-          <div className="flex gap-15 justify-center mt-10 py-2 px-2 bg-[rgba(71,68,68,0.75)] w-fit rounded-4xl items-center text-gray-100">
-            <img
-              className="rounded-4xl cursor-pointer w-10 h-10"
-              src={ImageBg}
-              alt="profile"
-            />
-            <ul className="uppercase flex gap-15 items-cener text-text">
-              <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl hover:shadow-xl">
-                Expertise
-              </li>
-              <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl">
-                My Work
-              </li>
-              <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl">
-                tech stack
-              </li>
-              <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl">
-                Contact
-              </li>
-            </ul>
-          </div>
+    <>
+      <div
+        className={`w-full h-auto bg-[url('./assets/bg-image.svg')] bg-no-repeat bg-cover p-0 overflow-hidden`}
+      >
+        <div ref={hoverAnimation}>
+          <div className="absolute w-6 h-6 bg-white rounded-4xl hover-anime -z-100"></div>
         </div>
-        <section
-          className=" flex flex-col items-center justify-center h-screen  box gap-10 "
-          ref={container}
-        >
-          <h3 className="flex gap-2 border w-fit px-10 py-2 text-xl rounded-4xl h-fit good text-gray-600 items-center ">
-            <span className="w-5 h-5 bg-[#27d655] rounded-3xl open "></span>
-            I'm currently available
-          </h3>
-          <div className="text-white flex flex-col gap-4 w-1/2  justify-center items-center text-center">
-            <h1 className="flex gap-2 text-7xl blurred-text  my-name">
-              <span className="myname">Habtemariam</span>
-              <span className="father-name">Bereket</span>{" "}
-            </h1>
-            <h2 className="text-4xl blurred-text title">
-              Full-Stack Developer
-            </h2>
-            <h4 className="text-3xl blurred-text description">
-              I transform innovative ideas into powerful, intuitive web
-              applications using modern technologies.{" "}
-            </h4>
 
-            <div className="liquid-morph-element uppercase cursor-pointer mt-10">
-              <span>Get In touch</span>
+        <div className="flex flex-col w-full h-full items-center  covered-bg">
+          <div className="fixed left-1/2 flex justify-center transform -translate-x-1/2 w-fit z-1000">
+            <div className="flex gap-15 justify-center mt-10 py-2 px-2 bg-[rgba(71,68,68,0.75)] w-fit rounded-4xl items-center text-gray-100">
+              <a href="#home">
+                {" "}
+                <img
+                  className="rounded-4xl cursor-pointer w-10 h-10"
+                  src={ImageBg}
+                  alt="profile"
+                />
+              </a>
+              <ul className="uppercase flex gap-15 items-cener text-text">
+                <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl hover:shadow-xl">
+                  <a href="#expertise">Expertise</a>
+                </li>
+                <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl">
+                  <a href="#my-work">My Work</a>
+                </li>
+                <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl">
+                  <a href="#tech-stack">tech stack</a>
+                </li>
+                <li className="cursor-pointer py-2 px-3 hover:bg-[rgba(71,68,68,0.8)] duration-500 hover:border border-black rounded-2xl">
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
             </div>
           </div>
-        </section>
-        <h2 className="text-3xl text-white uppercase font-bold mb-10">
-          Some Of My Projects with image
-        </h2>
-        <div
-          className="flex w-full h-[100vh] overflow-hidden relative mb-10"
-          ref={scrollRef}
-          id="container_2"
-        >
-          <div className="newContainer h-[100vh] w-full">
-            <img
-              className="img-5 absolute w-full h-screen"
-              src="/portifolio/13.png"
-              alt=""
-            />
-            <img
-              className="img-1 absolute w-full h-screen"
-              src="/portifolio/16.png"
-              alt=""
-            />
-            <img
-              className="img-2 absolute w-full h-screen"
-              src="/portifolio/2.png"
-              alt=""
-            />
-            <img
-              className="img-3 absolute w-full h-screen"
-              src="/portifolio/18.png"
-              alt=""
-            />
-            <img
-              className="img-4 absolute w-full h-screen"
-              src="/portifolio/5.png"
-              alt=""
-            />
-          </div>
-        </div>
-        <MyExpertise />
-        <section className="flex flex-col items-center  w-full h-full mb-10">
-          <h1 className="text-white text-3xl mb-10">My Works</h1>
-          <div className="grid grid-cols-3 w-1/2 h-[60vh] gap-10 text-[#5f6361] relative">
-            {Projects.map((project) => (
-              <div className="flex flex-col p-2 h-3/5 rounded-2xl border-4 border-[#5f6361] duration-300  Expretise-card">
-                <img
-                  className="w-full h-50 rounded-t-2xl"
-                  src={project.img}
-                  alt=""
-                />
-                <h2 className="text-xl text-white my-2">{project.title}</h2>
-                <div className="mt-auto mb-3">
-                  <p>{project.desc}</p>
-                  <div className="flex text-blue-700 justify-between">
-                    <a className="" href={project.frontend}>
-                      frontend
-                    </a>
 
-                    <a href={project.backend}>backend</a>
-                  </div>
-                </div>
-              </div>
-            ))}
+          <section
+            className=" flex flex-col items-center justify-center h-screen  box gap-10 "
+            ref={container}
+            id="home"
+          >
+            <h3 className="flex gap-2 border w-fit px-10 py-2 text-xl rounded-4xl h-fit good text-gray-600 items-center ">
+              <span className="w-5 h-5 bg-[#27d655] rounded-3xl open "></span>
+              I'm currently available
+            </h3>
+            <div className="flex flex-col gap-4 w-1/2 text-white  justify-center items-center text-center">
+              <h1 className="flex gap-2 text-7xl blurred-text  my-name">
+                <span className="myname">Habtemariam</span>
+                <span className="father-name">Bereket</span>{" "}
+              </h1>
+              <h2 className="text-4xl blurred-text title">
+                Full-Stack Developer
+              </h2>
+              <h4 className="text-3xl blurred-text description">
+                I transform innovative ideas into powerful, intuitive web
+                applications using modern technologies.{" "}
+              </h4>
+
+              <Button className="uppercase text-white w-50 h-16 text-2xl cursor-pointer bg-[#ff4d00] hover:bg-[#ff3a00] mt-10">
+                <a href="mailto:habteshbeki@gmail.com">Get in touch</a>
+              </Button>
+            </div>
+          </section>
+          <h2 className="text-3xl text-white uppercase font-bold mb-10">
+            Some Of My Projects with image
+          </h2>
+          <div
+            className="flex w-full h-[100vh] overflow-hidden relative mb-10"
+            ref={scrollRef}
+            id="container_2"
+          >
+            <div className="newContainer h-[100vh] w-full">
+              <img
+                className="img-5 absolute w-full h-screen"
+                src="/portifolio/13.png"
+                alt=""
+              />
+              <img
+                className="img-1 absolute w-full h-screen"
+                src="/portifolio/16.png"
+                alt=""
+              />
+              <img
+                className="img-2 absolute w-full h-screen"
+                src="/portifolio/2.png"
+                alt=""
+              />
+              <img
+                className="img-3 absolute w-full h-screen"
+                src="/portifolio/18.png"
+                alt=""
+              />
+              <img
+                className="img-4 absolute w-full h-screen"
+                src="/portifolio/5.png"
+                alt=""
+              />
+            </div>
           </div>
-        </section>
+          <MyExpertise />
+          <MyWork />
+          <section
+            className=" w-full flex flex-col items-center gap-6"
+            ref={logoRef}
+          >
+            <div>
+              <h1 className="text-4xl font-bold text-white ">Tech-Stack</h1>
+            </div>
+            <div
+              className="logo-animation grid w-1/2 h-[60vh] relative overflow-hidden"
+              id="tech-stack"
+            >
+              <div className="first flex gap-10 absolute">
+                {SecondLineImagae.map((img) => (
+                  <img className="w-20 h-20" src={img} />
+                ))}
+              </div>
+              <div className="second flex gap-10 absolute top-30 ">
+                {firstLineImagae.map((img) => (
+                  <img className="w-20 h-20" src={img} />
+                ))}
+              </div>
+              <div className="thried flex gap-10 top-60 absolute">
+                {ThiredLineImagae.map((img) => (
+                  <img className="w-20 h-20" src={img} />
+                ))}
+              </div>
+            </div>
+          </section>
+          <footer
+            className="w-full flex justify-center bg-[#03070e]"
+            id="contact"
+          >
+            <div
+              className=" flex flex-col items-center h-[55vh] w-1/2 mt-10 text-[#7c7e81] gap-8 text-center"
+              ref={containerFooter}
+            >
+              <h3 className="flex gap-2 border w-fit px-10 py-2 text-xl rounded-4xl h-fit good text-gray-600 items-center ">
+                <span className="w-5 h-5 bg-[#27d655] rounded-3xl footer-open-to-work "></span>
+                I'm currently available
+              </h3>
+              <p>
+                Got an idea? Starting a project? Need to chat? Let’s connect!
+              </p>
+              <h2 className="text-6xl">
+                Reach out, and let’s create something amazing together!
+              </h2>
+              <h1>
+                {} {}
+              </h1>
+              <Button className="uppercase text-white w-50 h-16 text-2xl cursor-pointer bg-[#ff4d00] hover:bg-[#ff3a00] mt-10">
+                <a href="https://t.me/HabtemariamB">Get in touch</a>
+              </Button>
+
+              <ul className="flex gap-3">
+                <li>
+                  <a href="">
+                    <img
+                      className="w-10 h-10 cursor-pointer hover:scale-125"
+                      src="/social-media/gmail.png"
+                      alt="gmail"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <img
+                      className="w-10 h-10 cursor-pointer hover:scale-125"
+                      src="/social-media/telegram.png"
+                      alt="telegram"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <img
+                      className="w-10 h-10 cursor-pointer hover:scale-125"
+                      src="/social-media/linkedin.png"
+                      alt="linkedin"
+                    />
+                  </a>
+                </li>
+              </ul>
+              <footer className="text-[#7c7e81] flex gap-2">
+                <Copyright /> <p>2025 Habtemariam Bereket</p>
+              </footer>
+            </div>
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,20 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+// export default defineConfig({
+//   plugins: [tailwindcss()],
+// });
+
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
