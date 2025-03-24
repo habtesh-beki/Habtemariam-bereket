a9f6e753eb5830e2a82b34f22b53abcd87d4ef13
habtesh-beki <habteshbaki2014@gmail.com>
2025-03-24 13:12:45 -0400

    I make it more responisve

--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -10,14 +10,14 @@ const Projects = [
     img: "/portifolio/13.png",
     title: "Ticketing System",
     desc: "I've done Ticketing system user can create ticket and admin can update the status of it",
-    frontend: "",
+    frontend: "https://github.com/habtesh-beki/ticketing_system_client.git",
     backend: "",
   },
   {
     img: "/portifolio/16.png",
     title: "Simple blogging app",
     desc: "We've done blogging app user can create blog and read others blog",
-    frontend: "",
+    frontend: "https://github.com/habtesh-beki/simple_bloging_app_client.git",
     backend: "",
   },
 ];
@@ -31,7 +31,7 @@ export default function MyWork() {
       <h1 className="text-white text-3xl mb-10">My latest Works</h1>
       <div className="grid lg:grid-cols-3 lg:w-1/2 md:w-3/4 sm:w-full h-max gap-10 text-[#5f6361] relative mx-4">
         {Projects.map((project) => (
-          <div className="flex flex-col p-2 lg:h-90 md:90 h-max rounded-2xl border-2 border-[#5f6361] duration-300  Expretise-card-1 overflow-hidden">
+          <div className="flex flex-col p-2 lg:h-95 md:90 h-max rounded-2xl border-2 border-[#5f6361] duration-300  Expretise-card-1 overflow-hidden">
             <img
               className="w-max h-max rounded-t-2xl"
               src={project.img}
@@ -40,13 +40,18 @@ export default function MyWork() {
             <h2 className="text-xl text-white my-2">{project.title}</h2>
             <div className="mt-auto mb-3">
               <p>{project.desc}</p>
-              <div className="flex text-blue-700 justify-between">
-                <a className="" href={project.frontend}>
-                  frontend
-                </a>
-
-                <a href={project.backend}>backend</a>
-              </div>
+              <a className="" href={project.frontend}>
+                {" "}
+                <div className="bg-white flex gap-2 w-max h-max px-3 py-1 rounded-md my-2">
+                  <img
+                    className="h-6 w-6 rounded-4xl"
+                    src="/tech-stack/GitHub.png"
+                    alt="github"
+                  />
+                  <span className="text-black">source</span>
+                  {/* <a href={project.backend}>backend</a> */}
+                </div>{" "}
+              </a>
             </div>
           </div>
         ))}
